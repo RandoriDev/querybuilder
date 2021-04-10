bd5ee441723712ac13207922e1fa37b7e604a4e9
BJ Swope <bigblueswope+github+public@gmail.com>
2021-04-10 13:39:19 -0400

    pylint cleanup

--- site_builder.py
+++ site_builder.py
@@ -1,11 +1,11 @@
+import argparse
 import copy
+import json
 import os
 import re
-import sys
 import shutil
 import subprocess
-import argparse
-import json
+import sys
 
 assert sys.version_info >= (3, 0), "This script requires the use of Python 3"
 
@@ -56,10 +56,6 @@ ep_wo_confidence_rule='''var rules_REPLACEME = {
   ]
 };
 '''
-
-
-
-
 
 
 def copytree(src, dst, symlinks=False, ignore=None):
@@ -116,28 +112,32 @@ def craft_selectize_string(field_name, picklist_data_type):
     return ss
 
 
-def build_website(api_file, output_dir):
+def process_api_file():
+
+
+
+def build_website():
     with open('templates/template-index-start.html', 'r') as e:
         html_str_1 = e.read()
-    
+
     with open('templates/template-index-middle.html', 'r') as e:
         html_str_2 = e.read()
-    
-    with open('templates/template-index-end.html', 'r') as e: 
-        html_str_3 = e.read() 
-        
+
+    with open('templates/template-index-end.html', 'r') as e:
+        html_str_3 = e.read()
+
     sect_str = ''
-    
+
     js_sources_string = ''
-    
+
     with open(api_file, 'r') as af:
         datastore = json.load(af)
-        
+
     schemas = datastore['components']['schemas']
-    
+
     do_not_includes = ['querybuilder_rule_group_schema', 'querybuilder_rule_group_schema_2',
                        'querybuilder_rule_group_schema_3', 'querybuilder_rule_group_schema_4',
-                       'querybuilder_rule_schema', 'saved_views_model_custom_in', 
+                       'querybuilder_rule_schema', 'saved_views_model_custom_in',
                        'saved_views_patch_in']
 
     for dash_endpoint in sorted(schemas.keys()):
@@ -146,14 +146,16 @@ def build_website(api_file, output_dir):
 
             filters = []
 
-            schemas[dash_endpoint]['required']
+            if not schemas[dash_endpoint]['required']:
 
-            endpoint = dash_endpoint.replace('-', '_');
+                continue
+
+            endpoint = dash_endpoint.replace('-', '_')
 
             if endpoint in do_not_includes:
 
                 continue
-            
+
             sort_list = []
 
             specials = {
@@ -183,7 +185,7 @@ def build_website(api_file, output_dir):
             }
 
             for k,v in sorted(schemas[dash_endpoint]['properties'].items()):
-                
+
                 # skip deleted because it is not cust facing
                 # skip all_ports because it is an array and jquery Querybuilder does not support array types
                 if k in ['deleted', 'all_ports']:
@@ -208,38 +210,38 @@ def build_website(api_file, output_dir):
 
                     pass
 
-                        
+
 
                 if k in specials.keys():
 
                     filters.append(k)
 
                     continue
-                    
+
                 filter_dict = {}
-                
+
                 filter_dict['id'] = f'table.{k}'
-                
+
                 filter_dict['label'] = labelize(k)
-                
+
                 if v['type'] == 'number':
                     filter_dict['type'] = 'double'
                 else:
                     filter_dict['type'] = v['type']
-        
+
                 if k in ['first_seen', 'last_seen' ]:
                     filter_dict['type'] = 'date'
                     filter_dict['validation'] = {'format': 'MM/DD/YYYY'}
                     filter_dict['plugin'] = 'datepicker'
-                    filter_dict['plugin_config'] = { 
-                                                'format': 'mm/dd/yyyy', 
-                                                'todayBtn': 'linked', 
-                                                'todayHighlight': True, 
+                    filter_dict['plugin_config'] = {
+                                                'format': 'mm/dd/yyyy',
+                                                'todayBtn': 'linked',
+                                                'todayHighlight': True,
                                                 'autoclose': True }
-        
+
                 filters.append(filter_dict)
-            
-            
+
+
             filt_string = json.dumps(filters, indent=2).replace('"','\'')
 
             picklist_str = '\n'
@@ -252,12 +254,12 @@ def build_website(api_file, output_dir):
 
                 new_str = craft_selectize_string(special, picklist_data_type)
 
-                foo = f"  '{special}'"
+                spec_str = f"  '{special}'"
 
-                tmpstr = re.sub(foo, new_str, filt_string)
+                tmpstr = re.sub(spec_str, new_str, filt_string)
 
                 filt_string = copy.deepcopy(tmpstr)
-            
+
             if endpoint == 'hostname':
 
                 def_rules_str = hostname_default_rule
@@ -270,20 +272,20 @@ def build_website(api_file, output_dir):
 
                 def_rules_str = default_rule
 
-            
+
             with open('templates/template-javascript.js', 'r') as j:
                 js_source_code = j.read().rstrip('\n')
-            
-            js_str = ''.join([picklist_str, def_rules_str, js_source_code, 
+
+            js_str = ''.join([picklist_str, def_rules_str, js_source_code,
                                 filt_string, '\n});'])
-            
+
             js_str = js_str.replace('REPLACEME', endpoint)
-            
+
             output_filename = f'{output_dir}/js/randori/{endpoint}.js'
-            
+
             with open(output_filename, 'w+') as o:
                 o.write(js_str)
-            
+
             sort_str = ', '.join(sort_list)
 
             sort_str = f'{sort_str}\n<section>\n'
@@ -291,46 +293,43 @@ def build_website(api_file, output_dir):
             with open('templates/template-index-section.html', 'r') as f:
                 sect_str = sect_str + f.read().replace('REPLACEME', endpoint)
                 sect_str = sect_str + sort_str
-            
+
             js_sources_string = js_sources_string + \
                 '<script src="js/randori/{}.js"></script>\n'.format(endpoint)
 
         except KeyError:
             pass
-            
-    
-    full_page = ''.join([html_str_1, sect_str, html_str_2, 
+
+
+    full_page = ''.join([html_str_1, sect_str, html_str_2,
                         js_sources_string, html_str_3])
-    
+
     index_outfile = f'{output_dir}/index.html'
 
     with open(index_outfile, 'w+') as o:
         o.write(full_page)
-        
+
     print ('Done')
-    
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description = '')
 
-    optional = parser._action_groups.pop()
-
     required = parser.add_argument_group('required arguments')
 
-    required.add_argument('-i', '--input', 
-                    required=True, 
+    required.add_argument('-i', '--input',
+                    required=True,
                     help='File containing the Randori API Spec.')
 
-    required.add_argument('-o', '--output', 
-                    required=True, 
+    required.add_argument('-o', '--output',
+                    required=True,
                     help='Directory in which to write the generated files.')
 
-    optional.add_argument('-s', '--setup', default=False, action='store_true',
+    parser.add_argument('-s', '--setup', default=False, action='store_true',
                             help='If the setup arg/flag is provided, copy the\
                             contents of the "framework" directory to the\
                             output directory.')
 
-    parser._action_groups.append(optional)
 
     args = parser.parse_args()
 
@@ -338,7 +337,7 @@ if __name__ == '__main__':
 
     output_dir = args.output
 
-    if not ( os.path.isdir(output_dir)):
+    if not os.path.isdir(output_dir):
         print ('{} does not exist.  Please create the directory and \
 run the script again.'.format(output_dir))
         sys.exit(1)
@@ -359,9 +358,9 @@ and run the script again.'.format(output_dir))
             with open('/etc/selinux/config', 'r') as sel:
                 if 'SELINUX=enforcing' in sel.read():
                     try:
-                        subprocess.run(['restorecon', '-r', output_dir])
+                        subprocess.run(['restorecon', '-r', output_dir], check=True)
                     except FileNotFoundError:
                         pass
 
-    build_website(api_file, output_dir)
+    build_website()
 
